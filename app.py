68437abc383320aba827e5bdfc135bd46d541fcb
Luigi Longo <luigicl@gmail.com>
2023-12-14 20:26:19 -0300

    Incrementado o estilo via bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+vagas = [
+    {
+        'id': 1,
+        'titulo': 'Analista de Dados',
+        'localidade': 'SC, Brasil',
+        'salario': 'R$ 5.000'
+    },
+    {
+        'id': 2,
+        'titulo': 'Engenheiro de Dados',
+        'localidade': 'SP, Brasil',
+        'salario': 'R$ 10.000'
+    },
+    {
+        'id': 3,
+        'titulo': 'Desenvolvedor Frontend',
+        'localidade': 'SP, Brasil',
+        'salario': 'R$ 3.000'
+    },
+    {
+        'id': 4,
+        'titulo': 'Desenvolvedor Backend',
+        'localidade': 'RJ, Brasil',
+        'salario': 'R$ 4.000'
+    },
+    {
+        'id': 5,
+        'titulo': 'Desenvolvedor Python',
+        'localidade': 'SP, Brasil',
+        'salario': 'R$ 50.000'
+    }
+]
+
+
 @app.route("/")
 def homepage():
-    return render_template("home.html")
+    return render_template("home.html", vagas=vagas)
 
 
 if __name__ == "__main__":
