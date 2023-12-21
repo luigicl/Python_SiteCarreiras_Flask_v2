135939af984c394ea5384f7b35226b1fa85d0f60
Luigi Longo <luigicl@gmail.com>
2023-12-21 15:17:21 -0300

    Acrescentado carregamento de vagas via BD MySQL

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import sqlalchemy
 from sqlalchemy import create_engine
 
 database = "python_sitecarreiras_flask"
-username = "0sw87txesopu60wgrqf6"
+username = "no0m6cfwb9kl71wtj5no"
 host = "aws.connect.psdb.cloud"
-password = "pscale_pw_e43NgZUO9oi5P2bGZ2aXFW1oEsFlBazvB6k0tQ3OIpB"
+password = "pscale_pw_b7A9Lw3EBU1wox9alzcJSv3pSg0Zu2yNIy0WNIQ5mf9"
 
 db_connection_string = f"mysql+pymysql://{username}:{password}@{host}/{database}?charset=utf8mb4"
 
